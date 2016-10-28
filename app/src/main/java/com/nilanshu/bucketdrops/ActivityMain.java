95a336ae210fe81ef0293f40d6d16e67b23a9f93
nilanshu96 <rajmanenilanshu@gmail.com>
2016-10-28 23:58:58 +0530

    Testing Image Scaling. Code contains the 'matrix' type of scaling.

--- app/src/main/java/com/nilanshu/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/nilanshu/bucketdrops/ActivityMain.java
@@ -1,13 +1,21 @@
 package com.nilanshu.bucketdrops;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class ActivityMain extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logo = (ImageView) findViewById(R.id.iv_logo);
+        Matrix matrix = new Matrix();
+        matrix.setRotate(10.0f);
+        logo.setImageMatrix(matrix);
     }
 }
